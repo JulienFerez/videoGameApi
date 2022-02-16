fc1acc2821917d235540e5bd0fe10adcb4c25ac0
Julien Ferez <Julienferez@icloud.com>
2022-02-16 16:32:53 +0100

    commit 16h30

--- src/index.ts
+++ src/index.ts
@@ -17,18 +17,6 @@ app.use(express.static("public"));
 app.get("/", (request, response) => {
   response.render("home");
 });
-/*// AFFICHER LES DIFFERENTES PLATEFORMES
-app.get("/game", (httpreq, resp) => {
-  request("http://videogame-api.fly.dev/games/platforms/<platform_id>", (error, body) => {
-    if (error) {
-      throw error;
-    }
-    const games = JSON.parse(body).games;
-    console.log(games, "Line 28");
-    resp.render("platform", { games });
-  });
-});
-*/
 
 // AFFICHER LES DIFFERENTES PLATFORMS
 app.get("/platform", (httpreq, resp) => {
@@ -43,14 +31,17 @@ app.get("/platform", (httpreq, resp) => {
 });
 
 // AFFICHER DIFFERENTS JEUX EN FONCTION DE LA PLATEFORME
-app.get("/platforms/:game", (httprequest, resp) => {
-  request(`https://videogame-api.fly.dev/games/platforms/${game}`, (error, body) => {
+
+app.get("/platform/:id", (httpRequest, response) => {
+  const id = httpRequest.params.id;
+  console.log(id);
+
+  request(`http://videogame-api.fly.dev/games/platforms/${id}`, (error, body) => {
     if (error) {
       throw error;
     }
     const games = JSON.parse(body).games;
-    // console.log(games);
-    resp.render("game", { games });
+    response.render("games", { games });
   });
 });
 
